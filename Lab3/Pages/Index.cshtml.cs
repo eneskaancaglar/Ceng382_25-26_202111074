da10c34c935979a8306c21ccd57cf1590b1300b2
eneskaancaglar <caglareneskaan@gmail.com>
2026-03-26 00:56:34 +0300

    week3

--- Lab3/Pages/Index.cshtml.cs
+++ Lab3/Pages/Index.cshtml.cs
@@ -1,87 +1,46 @@
-using Lab3.Services;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Lab3.Pages;
+namespace MyRazorAuthApp.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly IWebHostEnvironment _environment;
-
-    public IndexModel(IWebHostEnvironment environment)
-    {
-        _environment = environment;
-    }
-
-    public List<UserItem> Users { get; set; } = new List<UserItem>();
-    public List<string> CSharpImages { get; set; } = new List<string>();
+    public string[] StudentNames { get; set; } = [];
+    public int[] MidtermGrades { get; set; } = [];
+    public int[] FinalGrades { get; set; } = [];
+    public string[] LetterGrades { get; set; } = [];
 
     public void OnGet()
     {
-        LoadUsersFromFile();
-        LoadSavedImages();
+        StudentNames = new[] { "Ayşe", "Mehmet", "Zeynep" };
+        MidtermGrades = new[] { 70, 55, 90 };
+        FinalGrades = new[] { 80, 60, 75 };
+        LetterGrades = new[] { "-", "-", "-" };
     }
 
-    public async Task OnPostUploadCSharpAsync(List<IFormFile> CSharpFiles)
+    public void OnPostCalculate()
     {
-        LoadUsersFromFile();
+        StudentNames = new[] { "Ayşe", "Mehmet", "Zeynep" };
+        MidtermGrades = new[] { 70, 55, 90 };
+        FinalGrades = new[] { 80, 60, 75 };
 
-        string uploadFolder = Path.Combine(_environment.WebRootPath, "uploads");
+        LetterGrades = new string[StudentNames.Length];
 
-        if (!Directory.Exists(uploadFolder))
+        for (int i = 0; i < StudentNames.Length; i++)
         {
-            Directory.CreateDirectory(uploadFolder);
-        }
+            double average = MidtermGrades[i] * 0.40 + FinalGrades[i] * 0.60;
 
-        if (CSharpFiles != null && CSharpFiles.Count > 0)
-        {
-            foreach (var file in CSharpFiles)
-            {
-                if (file.Length > 0)
-                {
-                    string extension = Path.GetExtension(file.FileName).ToLower();
-                    var allowedExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif", ".webp" };
-
-                    if (!allowedExtensions.Contains(extension))
-                    {
-                        continue;
-                    }
-
-                    string uniqueFileName = Guid.NewGuid().ToString() + extension;
-                    string filePath = Path.Combine(uploadFolder, uniqueFileName);
-
-                    using (var stream = new FileStream(filePath, FileMode.Create))
-                    {
-                        await file.CopyToAsync(stream);
-                    }
-                }
-            }
-        }
-
-        LoadSavedImages();
-    }
-
-    private void LoadUsersFromFile()
-    {
-        string usersFilePath = Path.Combine(_environment.ContentRootPath, "App_Data", "users.json");
-        Users = UserStorage.GetUsers(usersFilePath);
-    }
-
-    private void LoadSavedImages()
-    {
-        string uploadFolder = Path.Combine(_environment.WebRootPath, "uploads");
-
-        CSharpImages = new List<string>();
-
-        if (Directory.Exists(uploadFolder))
-        {
-            var files = Directory.GetFiles(uploadFolder);
-
-            foreach (var file in files)
-            {
-                string fileName = Path.GetFileName(file);
-                CSharpImages.Add("/uploads/" + fileName);
-            }
+            if (average >= 90)
+                LetterGrades[i] = "AA";
+            else if (average >= 85)
+                LetterGrades[i] = "BA";
+            else if (average >= 80)
+                LetterGrades[i] = "BB";
+            else if (average >= 70)
+                LetterGrades[i] = "CB";
+            else if (average >= 60)
+                LetterGrades[i] = "CC";
+            else
+                LetterGrades[i] = "FF";
         }
     }
 }
