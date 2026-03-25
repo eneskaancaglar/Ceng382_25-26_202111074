c046cfd1a95e14262654ecdee7895d67c4422816
eneskaancaglar <caglareneskaan@gmail.com>
2026-03-26 01:38:56 +0300

    register sistemi oluşturuldugörseller eklendi 4 parçalı sayfa tamamlandı

--- Lab3/obj/Release/net10.0/Lab3.AssemblyInfo.cs
+++ Lab3/obj/Release/net10.0/Lab3.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f181c8dce5ce87a6474ef54dd93cbd49f7cf6240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b70c686f5863db07ff6e7736c420ae90db091167")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
